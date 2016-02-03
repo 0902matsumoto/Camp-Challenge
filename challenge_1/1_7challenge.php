b0a20030bcca0f57162b853ef9cb0b9615d330f4
Takuya Matsumoto <0902matsumoto@gmail.com>
2016-02-04 03:41:09 +0900

    課題要件を満たしていないかもしれないですが、一旦、PHP基礎学習２の課題を進めます。

---
+++ challenge_1/1_7challenge.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC"-//WSC//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>PHP基礎</title>
+</head>
+<boby>
+
+<?php
+$total=$_GET['total'];
+$name=$_GET['name'];
+$yen=$_GET['yen'];
+
+if($name==1){print'雑貨';}
+elseif($name==2){print'生鮮食品';}
+else{print'その他';}
+print'<br/>';
+
+$x=$yen/$total;
+print"$x".'<br/>';
+
+if($yen>=3000){print'4%';}
+if($yen>=5000){print'5%';}
+
+?>
+
+
+</body>
+</html>
