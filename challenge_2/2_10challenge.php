0aaf790375df82cfe8346221df070121ff2ff714
Takuya Matsumoto <0902matsumoto@gmail.com>
2016-02-06 15:11:22 +0900

    while() の（）内の＝＝の条件式を、＝と勘違いしていた。調べ、作成しました。

---
+++ challenge_2/2_10challenge.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML PUBLIC"-//WSC//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>PHP基礎</title>
+</head>
+<boby>
+
+<?php
+
+$x=$_GET['x'];
+
+print"$x".'<br/>';
+while($x%2==0)
+{
+$x=$x/2;
+print 2;
+print "<br/>";
+$i=$x%2;
+}
+
+while($x%3==0)
+{
+$x=$x/3;
+print 3;
+print "<br/>";
+$i=$x%3;
+}
+
+while($x%5==0)
+{
+$x=$x/5;
+print 5;
+print "<br/>";
+$i=$x%5;
+}
+
+while($x%7==0)
+{
+$x=$x/7;
+print 7;
+print "<br/>";
+$i=$x%7;
+}
+
+if($x!=1)print'その他';
+
+
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
