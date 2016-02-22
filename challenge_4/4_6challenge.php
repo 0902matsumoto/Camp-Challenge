5190778710a06af96d1521a0a0bcfbe602b16386
matsumoto <0902matsumoto@gmail.com>
2016-02-22 23:03:19 +0900

    課題修正しました

---
+++ challenge_4/4_6challenge.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC"-//WSC//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>PHP基礎</title>
+</head>
+<boby>
+
+<?php
+//６．自分のメールアドレスの「@」以降の文字を取得して、表示してください。
+
+$moji=strstr('0902matsumoto.@gmail.com','g');
+
+print"$moji";
+
+?>
+
+</body>
+</html>
